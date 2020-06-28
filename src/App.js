dffcd45d5ae24b532f2d787acd12d3bd725f508b
Ron Welch <rwelch@acm.org>
2020-06-28 13:39:46 -0400

    Cosmetic change.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     return (
       <form onSubmit={this.loginUser}>
         <h1>An Awesome Form on React</h1>
+        <h2>Updated by CircleCI</h2>
         <label htmlFor="email">
           <span>Email:</span>
           <input onChange={this.inputChange} value={email} id="email" name="email" type="email" />
